02418e4dfcff7e8e16a5f7cad19adbeb06eed479
ShahromDehoti <shahrom.dehoti1999@gmail.com>
2025-02-27 21:54:17 -0800

    added first name to User model

--- website/models.py
+++ website/models.py
@@ -7,6 +7,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(120), nullable=False)
     # You could add more fields as needed (e.g. username)
 
 class Photo(db.Model):
